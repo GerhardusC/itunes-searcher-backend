65040a00198474a8d8d4d3a13e7a8a437c99e8da
Gerhardus <hardus.cronje@gmail.com>
2023-07-19 18:15:03 +0200

    Cleaned formatting

--- fetcher.test.js
+++ fetcher.test.js
@@ -1,28 +1,32 @@
 //The function that fetches data from the API to be tested.
 const getResults = async (searchTerm, mediaType) => {
-    let searchResults = {}
-    if(searchTerm && mediaType) {
-        try {
-            let result = await fetch(`https://itunes.apple.com/search?term=${searchTerm}&media=${mediaType}`)
-            searchResults = await result.json()
-        } catch (err) {
-            console.log(err)
-        } finally {
-            return searchResults
-        }
+  let searchResults = {};
+  if (searchTerm && mediaType) {
+    try {
+      let result = await fetch(
+        `https://itunes.apple.com/search?term=${searchTerm}&media=${mediaType}`
+      );
+      searchResults = await result.json();
+    } catch (err) {
+      console.log(err);
+    } finally {
+      return searchResults;
     }
-    if(searchTerm) {
-        try {
-            let result = await fetch(`https://itunes.apple.com/search?term=${searchTerm}`)
-            searchResults = await result.json()
-        } catch (err) {
-            console.log(err)
-        } finally {
-            return searchResults
-        }
+  }
+  if (searchTerm) {
+    try {
+      let result = await fetch(
+        `https://itunes.apple.com/search?term=${searchTerm}`
+      );
+      searchResults = await result.json();
+    } catch (err) {
+      console.log(err);
+    } finally {
+      return searchResults;
     }
-}
+  }
+};
 //Check if an object is returned.
-test('Fetched data.', () => { 
-    expect(typeof(getResults("James blunt", "all"))).toBe("object")
- })
+test("Fetched data.", () => {
+  expect(typeof getResults("James blunt", "all")).toBe("object");
+});
